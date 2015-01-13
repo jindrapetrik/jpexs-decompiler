677bdf969bb5e7a56fefc3766ed773758edbab6a
honfika@gmail.com <honfika@gmail.com>
2015-01-13 19:36:39 +0100

    copyright year updated

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/treeitems/ScriptContainer.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/treeitems/ScriptContainer.java
@@ -1,8 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ *  Copyright (C) 2010-2015 JPEXS, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. */
 package com.jpexs.decompiler.flash.treeitems;
 
 import java.util.Map;
--- src/com/jpexs/decompiler/flash/gui/LicenseUpdater.java
+++ src/com/jpexs/decompiler/flash/gui/LicenseUpdater.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010-2014 JPEXS
+ *  Copyright (C) 2010-2015 JPEXS
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -67,7 +67,7 @@ public class LicenseUpdater {
         
          */
         int defaultStartYear = 2010;
-        int defaultFinalYear = 2014;
+        int defaultFinalYear = 2015;
         String defaultAuthor = "JPEXS";
         String defaultYearStr = Integer.toString(defaultStartYear);
         if (defaultFinalYear != defaultStartYear) {
