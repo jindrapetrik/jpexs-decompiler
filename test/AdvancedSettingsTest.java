54f61ef862d29e3e96541915018b90b2915e07f3
honfika@gmail.com <honfika@gmail.com>
2015-05-06 15:56:54 +0200

    advanced settigns test (not finished)

---
+++ test/AdvancedSettingsTest.java
@@ -0,0 +1,33 @@
+
+import com.jpexs.decompiler.flash.gui.AppStrings;
+import com.jpexs.decompiler.flash.gui.MainFrame;
+import org.testng.annotations.Test;
+
+/*
+ *  Copyright (C) 2010-2015 JPEXS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ * @author JPEXS
+ */
+public class AdvancedSettingsTest {
+
+    @Test
+    public void testAdvancedSettginsDialog() {
+        AppStrings.setResourceClass(MainFrame.class);
+        //new AdvancedSettingsDialog();
+    }
+}
