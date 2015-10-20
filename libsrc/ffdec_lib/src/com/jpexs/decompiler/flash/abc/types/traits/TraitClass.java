1a55deb890036f50e8b5680cd85021d9140dcbd4
honfika@gmail.com <honfika@gmail.com>
2015-10-20 19:29:29 +0200

    as code format fix

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/abc/types/traits/TraitClass.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/abc/types/traits/TraitClass.java
@@ -474,8 +474,6 @@ public class TraitClass extends Trait implements TraitWithSlot {
                 writer.startBlock();
                 abc.bodies.get(bodyIndex).toString(path +/*packageName +*/ "/" + instanceInfoName + ".staticinitializer", exportMode, abc, this, abc.constants, abc.method_info, writer, fullyQualifiedNames);
                 writer.endBlock();
-            } else {
-                writer.append(" ");
             }
             writer.endMethod();
             writer.endTrait();
