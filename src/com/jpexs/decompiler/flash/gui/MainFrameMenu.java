742412065a79054624e853f8e9115013446a1492
Jindra Petřík <jindra.petrik@gmail.com>
2015-11-23 16:20:43 +0100

    debug menu item enabled fix

--- src/com/jpexs/decompiler/flash/gui/MainFrameMenu.java
+++ src/com/jpexs/decompiler/flash/gui/MainFrameMenu.java
@@ -727,7 +727,7 @@ public abstract class MainFrameMenu implements MenuBuilder {
         setMenuEnabled("/help/about", !isWorking);
 
         setMenuEnabled("/file/start/run", swfSelected && !isRunningOrDebugging);
-        setMenuEnabled("/file/start/debug", !isRunningOrDebugging);
+        setMenuEnabled("/file/start/debug", swfSelected && !isRunningOrDebugging);
 
         setMenuEnabled("/file/start/stop", isRunningOrDebugging);
         setMenuEnabled("/debugging/debug/stop", isRunningOrDebugging); //same as previous
