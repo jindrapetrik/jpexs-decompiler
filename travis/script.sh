4bddfd37b791cbd0a61a22f291935776ece63bd2
Jindra Petřík <jindra.petrik@gmail.com>
2016-09-20 21:47:37 +0200

    reverse condition for nightly

--- travis/script.sh
+++ travis/script.sh
@@ -2,10 +2,10 @@
 # If we've got website password, we can upload nightly builds.
 # Travis secure variable $website_password is not available from outside 
 # of jpexs repository (e.g pull requests from other users on GitHub)
-if [ -z ${website_password+x} ]; then 
+if [ -z ${website_password+x} ]; then
+    # password not set,  just make private release without publishing result    
+    ant all
+else
     # create nightly build
     ant new-version-nightlybuild
-else
-    # just make private release without publishing result    
-    ant all
 fi
