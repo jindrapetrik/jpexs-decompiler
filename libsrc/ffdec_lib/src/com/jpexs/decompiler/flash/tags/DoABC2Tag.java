e9da1b291215bc00d4c6da70dbdf9a3401667f58
Jindra Petřík <jindra.petrik@gmail.com>
2021-02-15 20:55:51 +0100

    Do not show empty parenthesis in DoABC2 tag

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/tags/DoABC2Tag.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/tags/DoABC2Tag.java
@@ -12,7 +12,8 @@
  * Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library. */
+ * License along with this library.
+ */
 package com.jpexs.decompiler.flash.tags;
 
 import com.jpexs.decompiler.flash.SWF;
@@ -118,7 +119,7 @@ public class DoABC2Tag extends Tag implements ABCContainerTag {
 
     @Override
     public String getName() {
-        return super.getName() + " (" + name + ")";
+        return super.getName() + (!name.isEmpty() ? " (" + name + ")" : "");
     }
 
     @Override
