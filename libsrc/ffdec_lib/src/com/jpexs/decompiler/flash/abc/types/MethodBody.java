877d298d689407d75cb75c1c2d3bdf451c26465b
Jindra Petřík <jindra.petrik@gmail.com>
2021-02-28 08:12:00 +0100

    Fixed: #1633 AS3 decompilation - return in for..in clause

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/abc/types/MethodBody.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/abc/types/MethodBody.java
@@ -45,7 +45,6 @@ import com.jpexs.helpers.Helper;
 import com.jpexs.helpers.MemoryInputStream;
 import com.jpexs.helpers.stat.Statistics;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
