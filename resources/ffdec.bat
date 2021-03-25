941d0e031828efb57ac2605e60b9b99cbada0e2f
Jindra Petřík <jindra.petrik@gmail.com>
2021-03-25 20:53:09 +0100

    #1015, #1466, #1513 Better error messages during saving, display message on out of memory

--- resources/ffdec.bat
+++ resources/ffdec.bat
@@ -1,2 +1,5 @@
 @echo off
-java -Xmx1024m -Djna.nosys=true -Dsun.java2d.uiScale=1.0 -jar "%~dp0\ffdec.jar" %*
+rem Set following to higher value if you want more memory:
+rem You need 64 bit OS and 64 bit java to set it to higher values
+set MEMORY=1024m
+java -Xmx%MEMORY% -Djna.nosys=true -Dsun.java2d.uiScale=1.0 -jar "%~dp0\ffdec.jar" %*
