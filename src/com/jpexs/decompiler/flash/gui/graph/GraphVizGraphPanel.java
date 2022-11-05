bbbf62a6630111b0fa2a58eab504929b04cd81bb
Jindra Petřík <jindra.petrik@gmail.com>
2022-11-05 09:49:11 +0100

    Update license header year

--- src/com/jpexs/decompiler/flash/gui/graph/GraphVizGraphPanel.java
+++ src/com/jpexs/decompiler/flash/gui/graph/GraphVizGraphPanel.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016-2021 JPEXS
+ *  Copyright (C) 2016-2022 JPEXS
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
