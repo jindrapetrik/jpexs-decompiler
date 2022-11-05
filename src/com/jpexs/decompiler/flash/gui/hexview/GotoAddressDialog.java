1d82af4855d0fbb0fec21d920c54bb3f74043538
Jindra Petřík <jindra.petrik@gmail.com>
2022-11-05 09:49:11 +0100

    Update license header year

--- src/com/jpexs/decompiler/flash/gui/hexview/GotoAddressDialog.java
+++ src/com/jpexs/decompiler/flash/gui/hexview/GotoAddressDialog.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2021 JPEXS
+ *  Copyright (C) 2021-2022 JPEXS
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
