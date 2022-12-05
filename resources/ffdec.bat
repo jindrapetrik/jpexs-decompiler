e2628d6baf9afb45ec76c28814a054a3bcf3ad44
Jindra Petřík <jindra.petrik@gmail.com>
2022-12-05 21:01:25 +0100

    Hide VLC error output

--- resources/ffdec.bat
+++ resources/ffdec.bat
@@ -2,4 +2,8 @@
 rem Set following to higher value if you want more memory:
 rem You need 64 bit OS and 64 bit java to set it to higher values
 set MEMORY=1024m
+
+rem Hide VLC error output
+set VLC_VERBOSE=-1
+
 java -Xmx%MEMORY% -Djna.nosys=true -jar "%~dp0\ffdec.jar" %*
