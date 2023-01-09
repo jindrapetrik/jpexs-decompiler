414846679d4c63c9769fbd0db6ce0fa31a790773
Jindra Petřík <jindra.petrik@gmail.com>
2023-01-09 10:32:37 +0100

    Fixed CXFORM and GRADRECORD causing NotSerializableException

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/ConstantColorColorTransform.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/ConstantColorColorTransform.java
@@ -16,11 +16,13 @@
  */
 package com.jpexs.decompiler.flash.types;
 
+import java.io.Serializable;
+
 /**
  *
  * @author JPEXS
  */
-public class ConstantColorColorTransform extends ColorTransform {
+public class ConstantColorColorTransform extends ColorTransform implements Serializable {
 
     private final int color;
 
