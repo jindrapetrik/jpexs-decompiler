e28dc6820cd7fa419b412bbf982c9bee8e025455
Jindra Petřík <jindra.petrik@gmail.com>
2023-09-10 10:02:09 +0200

    Embed metadata samples contain related framework classes (static link)

--- libsrc/ffdec_lib/testdata/as3_embed/build_air_debug.bat
+++ libsrc/ffdec_lib/testdata/as3_embed/build_air_debug.bat
@@ -1,6 +1,6 @@
 @echo off
 set COMPILERKIND=air
-call c:\air\bin\mxmlc.bat -debug=true -output bin/as3_embed_attrib.%COMPILERKIND%.swf src/MainAttributesAir.as 1> buildlog.attrib.%COMPILERKIND%.txt 2>&1
-call c:\air\bin\mxmlc.bat -debug=true -output bin/as3_embed_classes.%COMPILERKIND%.swf src/MainClassesAir.as 1> buildlog.classes.%COMPILERKIND%.txt 2>&1
+call c:\air\bin\mxmlc.bat -debug=true -static-link-runtime-shared-libraries -output bin/as3_embed_attrib.%COMPILERKIND%.swf src/MainAttributesAir.as 1> buildlog.attrib.%COMPILERKIND%.txt 2>&1
+call c:\air\bin\mxmlc.bat -debug=true -static-link-runtime-shared-libraries -output bin/as3_embed_classes.%COMPILERKIND%.swf src/MainClassesAir.as 1> buildlog.classes.%COMPILERKIND%.txt 2>&1
 
 rem -warnings=false
--- libsrc/ffdec_lib/testdata/as3_embed/build_flex_debug.bat
+++ libsrc/ffdec_lib/testdata/as3_embed/build_flex_debug.bat
@@ -1,6 +1,6 @@
 @echo off
 set COMPILERKIND=flex
-c:\flex\bin\mxmlc.exe -debug=true -output bin/as3_embed_attrib.%COMPILERKIND%.swf src/MainAttributesFlex.as 1> buildlog.attrib.%COMPILERKIND%.txt 2>&1
-c:\flex\bin\mxmlc.exe -debug=true -output bin/as3_embed_classes.%COMPILERKIND%.swf src/MainClassesFlex.as 1> buildlog.classes.%COMPILERKIND%.txt 2>&1
+c:\flex\bin\mxmlc.exe -debug=true -static-link-runtime-shared-libraries -output bin/as3_embed_attrib.%COMPILERKIND%.swf src/MainAttributesFlex.as 1> buildlog.attrib.%COMPILERKIND%.txt 2>&1
+c:\flex\bin\mxmlc.exe -debug=true -static-link-runtime-shared-libraries -output bin/as3_embed_classes.%COMPILERKIND%.swf src/MainClassesFlex.as 1> buildlog.classes.%COMPILERKIND%.txt 2>&1
 
 rem -warnings=false
