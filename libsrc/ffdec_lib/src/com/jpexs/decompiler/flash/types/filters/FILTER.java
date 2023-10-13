0110e4e31e0b9003434fde568d6224a06dd8e252
Jindra Petřík <jindra.petrik@gmail.com>
2023-10-13 23:02:47 +0200

    Fixed SVG Frames export - filter

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/filters/FILTER.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/filters/FILTER.java
@@ -16,11 +16,17 @@
  */
 package com.jpexs.decompiler.flash.types.filters;
 
+import com.jpexs.decompiler.flash.exporters.commonshape.SVGExporter;
 import com.jpexs.decompiler.flash.types.BasicType;
+import com.jpexs.decompiler.flash.types.RGBA;
 import com.jpexs.decompiler.flash.types.annotations.SWFType;
 import com.jpexs.helpers.ConcreteClasses;
 import com.jpexs.helpers.SerializableImage;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Bitmap filter
@@ -50,4 +56,195 @@ public abstract class FILTER implements Serializable {
     public abstract double getDeltaX();
 
     public abstract double getDeltaY();
+
+    public String toSvg(Document document, Element filtersElement, SVGExporter exporter, String in) {
+        return null;
+    }
+
+    protected String dropShadowSvg(
+            double distance,
+            double angle,
+            RGBA dropShadowColor,
+            boolean innerShadow,
+            boolean knockout,
+            boolean compositeSource,
+            double blurX,
+            double blurY,
+            double strength,
+            int iterations,
+            Document document,
+            Element filtersElement,
+            SVGExporter exporter,
+            String in
+    ) {
+        double dx = distance * Math.cos(angle);
+        double dy = distance * Math.sin(angle);
+
+        if (innerShadow) {
+            Element feFlood = document.createElement("feFlood");
+            feFlood.setAttribute("flood-color", dropShadowColor.toHexRGB());
+            feFlood.setAttribute("flood-opacity", "" + dropShadowColor.getAlphaFloat());
+            filtersElement.appendChild(feFlood);
+            String feFloodResult = exporter.getUniqueId("filterResult");
+            feFlood.setAttribute("result", feFloodResult);
+
+            Element feOffset = document.createElement("feOffset");
+            feOffset.setAttribute("dx", "" + dx);
+            feOffset.setAttribute("dy", "" + dy);
+            feOffset.setAttribute("in", in);
+            String feOffsetResult = exporter.getUniqueId("filterResult");
+            feOffset.setAttribute("result", feOffsetResult);
+            filtersElement.appendChild(feOffset);
+
+            Element feComposite1 = document.createElement("feComposite");
+            feComposite1.setAttribute("in", feFloodResult);
+            feComposite1.setAttribute("in2", feOffsetResult);
+            feComposite1.setAttribute("operator", "out");
+            String feComposite1Result = exporter.getUniqueId("filterResult");
+            feComposite1.setAttribute("result", feComposite1Result);
+            filtersElement.appendChild(feComposite1);
+
+            String blurResult = blurSvg(blurX, blurY, iterations, document, filtersElement, exporter, feComposite1Result);
+
+            Element feComposite2 = document.createElement("feComposite");
+            feComposite2.setAttribute("in", blurResult);
+            feComposite2.setAttribute("in2", in);
+            feComposite2.setAttribute("operator", "in");
+
+            String result = exporter.getUniqueId("filterResult");
+            feComposite2.setAttribute("result", result);
+            filtersElement.appendChild(feComposite2);
+
+            if (Double.compare(strength, 1.0) != 0) {
+                Element feColorMatrix2 = document.createElement("feColorMatrix");
+                feColorMatrix2.setAttribute("type", "matrix");
+                feColorMatrix2.setAttribute("in", result);
+                feColorMatrix2.setAttribute("values",
+                        "1 0 0 0 0,"
+                        + "0 1 0 0 0,"
+                        + "0 0 1 0 0,"
+                        + "0 0 0 " + strength + " 0"
+                );
+                result = exporter.getUniqueId("filterResult");
+                feColorMatrix2.setAttribute("result", result);
+                filtersElement.appendChild(feColorMatrix2);
+            }
+
+            if (knockout || !compositeSource) {
+                return result;
+            }
+            Element feComposite3 = document.createElement("feComposite");
+            feComposite3.setAttribute("in", result);
+            feComposite3.setAttribute("in2", in);
+            feComposite3.setAttribute("operator", "over");
+
+            result = exporter.getUniqueId("filterResult");
+            feComposite3.setAttribute("result", result);
+            filtersElement.appendChild(feComposite3);
+            return result;
+        } else {
+            Element feOffset = document.createElement("feOffset");
+            feOffset.setAttribute("dx", "" + dx);
+            feOffset.setAttribute("dy", "" + dy);
+            feOffset.setAttribute("in", in);
+            String feOffsetResult = exporter.getUniqueId("filterResult");
+            feOffset.setAttribute("result", feOffsetResult);
+            filtersElement.appendChild(feOffset);
+
+            Element feColorMatrix = document.createElement("feColorMatrix");
+            feColorMatrix.setAttribute("type", "matrix");
+            feColorMatrix.setAttribute("in", feOffsetResult);
+            feColorMatrix.setAttribute("values",
+                    "0 0 0 0 " + (dropShadowColor.red / 255f) + ","
+                    + "0 0 0 0 " + (dropShadowColor.green / 255f) + ","
+                    + "0 0 0 0 " + (dropShadowColor.blue / 255f) + ","
+                    + "0 0 0 1 0"
+            );
+            String feColorMatrixResult = exporter.getUniqueId("filterResult");
+            feColorMatrix.setAttribute("result", feColorMatrixResult);
+            filtersElement.appendChild(feColorMatrix);
+
+            String result = blurSvg(blurX, blurY, iterations, document, filtersElement, exporter, feColorMatrixResult);
+
+            if (Double.compare(strength, 1.0) != 0) {
+                Element feColorMatrix2 = document.createElement("feColorMatrix");
+                feColorMatrix2.setAttribute("type", "matrix");
+                feColorMatrix2.setAttribute("in", result);
+                feColorMatrix2.setAttribute("values",
+                        "1 0 0 0 0,"
+                        + "0 1 0 0 0,"
+                        + "0 0 1 0 0,"
+                        + "0 0 0 " + strength + " 0"
+                );
+                result = exporter.getUniqueId("filterResult");
+                feColorMatrix2.setAttribute("result", result);
+                filtersElement.appendChild(feColorMatrix2);
+            }
+
+            if (!knockout && !compositeSource) {
+                return result;
+            }
+
+            Element feComposite = document.createElement("feComposite");
+            if (knockout) {
+                feComposite.setAttribute("in", result);
+                feComposite.setAttribute("in2", in);
+                feComposite.setAttribute("operator", "out");
+            } else {
+                feComposite.setAttribute("in", in);
+                feComposite.setAttribute("in2", result);
+                feComposite.setAttribute("operator", "over");
+            }
+
+            result = exporter.getUniqueId("filterResult");
+            feComposite.setAttribute("result", result);
+            filtersElement.appendChild(feComposite);
+            return result;
+        }
+    }
+
+    protected String blurSvg(double blurX, double blurY, int iterations, Document document, Element filtersElement, SVGExporter exporter, String in) {
+        if (iterations == 0) {
+            return in;
+        }
+        if (Double.compare(blurX, 0.0) == 0 && Double.compare(blurY, 0.0) == 0) {
+            return in;
+        }
+        Element element = document.createElement("feConvolveMatrix");
+
+        List<String> parts = new ArrayList<>();
+
+        double divisor;
+        if (Double.compare(blurX, 0.0) == 0) {
+            int orderX = (int) Math.ceil(blurX);
+            divisor = orderX;
+            element.setAttribute("order", "" + orderX + " 1");
+
+        } else if (Double.compare(blurY, 0.0) == 0) {
+            int orderY = (int) Math.ceil(blurY);
+            divisor = orderY;
+            element.setAttribute("order", "1 " + orderY);
+        } else {
+            int orderX = (int) Math.ceil(blurX);
+            int orderY = (int) Math.ceil(blurY);
+            divisor = orderX * orderY;
+            element.setAttribute("order", "" + orderX + " " + orderY);
+        }
+
+        for (int i = 0; i < divisor; i++) {
+            parts.add("1");
+        }
+
+        element.setAttribute("divisor", "" + divisor);
+
+        element.setAttribute("kernelMatrix", String.join(" ", parts));
+        element.setAttribute("in", in);
+
+        String result = exporter.getUniqueId("filterResult");
+        element.setAttribute("result", result);
+
+        filtersElement.appendChild(element);
+
+        return blurSvg(blurX, blurY, iterations - 1, document, filtersElement, exporter, result);
+    }
 }
--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/filters/GLOWFILTER.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/types/filters/GLOWFILTER.java
@@ -16,10 +16,14 @@
  */
 package com.jpexs.decompiler.flash.types.filters;
 
+import com.jpexs.decompiler.flash.SWF;
+import com.jpexs.decompiler.flash.exporters.commonshape.SVGExporter;
 import com.jpexs.decompiler.flash.types.BasicType;
 import com.jpexs.decompiler.flash.types.RGBA;
 import com.jpexs.decompiler.flash.types.annotations.SWFType;
 import com.jpexs.helpers.SerializableImage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Glow filter
@@ -92,5 +96,104 @@ public class GLOWFILTER extends FILTER {
     @Override
     public double getDeltaY() {
         return blurY;
+    }
+
+    @Override
+    public String toSvg(Document document, Element filtersElement, SVGExporter exporter, String in) {
+        /*Element element = document.createElement("feDropShadow");
+        element.setAttribute("dx", "0");
+        element.setAttribute("dy", "0");
+
+        double blur = ((blurX + blurY) / 2);
+        element.setAttribute("stdDeviation", "" + blur);
+
+        element.setAttribute("flood-color", glowColor.toHexRGB());
+        element.setAttribute("flood-opacity", "" + glowColor.getAlphaFloat());
+
+        element.setAttribute("in", in);
+        
+        String result = exporter.getUniqueId("filterResult");
+        element.setAttribute("result", result);
+        
+        filtersElement.appendChild(element);
+        
+        return result;*/
+
+        if (innerGlow) {
+            Element feFlood = document.createElement("feFlood");
+            feFlood.setAttribute("flood-color", glowColor.toHexRGB());
+            feFlood.setAttribute("flood-opacity", "" + glowColor.getAlphaFloat());
+            filtersElement.appendChild(feFlood);
+            String feFloodResult = exporter.getUniqueId("filterResult");
+            feFlood.setAttribute("result", feFloodResult);
+
+            Element feComposite1 = document.createElement("feComposite");
+            feComposite1.setAttribute("in", feFloodResult);
+            feComposite1.setAttribute("in2", in);
+            feComposite1.setAttribute("operator", "out");
+            String feComposite1Result = exporter.getUniqueId("filterResult");
+            feComposite1.setAttribute("result", feComposite1Result);
+            filtersElement.appendChild(feComposite1);
+
+            String blurResult = blurSvg(blurX, blurY, passes, document, filtersElement, exporter, feComposite1Result);
+
+            Element feComposite2 = document.createElement("feComposite");
+            feComposite2.setAttribute("in", blurResult);
+            feComposite2.setAttribute("in2", in);
+            feComposite2.setAttribute("operator", "in");
+
+            String feComposite2Result = exporter.getUniqueId("filterResult");
+            feComposite2.setAttribute("result", feComposite2Result);
+            filtersElement.appendChild(feComposite2);
+
+            if (knockout || !compositeSource) {
+                return feComposite2Result;
+            }
+            Element feComposite3 = document.createElement("feComposite");
+            feComposite3.setAttribute("in", feComposite2Result);
+            feComposite3.setAttribute("in2", in);
+            feComposite3.setAttribute("operator", "over");
+
+            String feComposite3Result = exporter.getUniqueId("filterResult");
+            feComposite3.setAttribute("result", feComposite3Result);
+            filtersElement.appendChild(feComposite3);
+            return feComposite3Result;
+        } else {
+
+            Element feColorMatrix = document.createElement("feColorMatrix");
+            feColorMatrix.setAttribute("type", "matrix");
+            feColorMatrix.setAttribute("in", in);
+            feColorMatrix.setAttribute("values",
+                    "0 0 0 0 " + (glowColor.red / 255f) + ","
+                    + "0 0 0 0 " + (glowColor.green / 255f) + ","
+                    + "0 0 0 0 " + (glowColor.blue / 255f) + ","
+                    + "0 0 0 1 0"
+            );
+            String feColorMatrixResult = exporter.getUniqueId("filterResult");
+            feColorMatrix.setAttribute("result", feColorMatrixResult);
+            filtersElement.appendChild(feColorMatrix);
+
+            String blurResult = blurSvg(blurX, blurY, passes, document, filtersElement, exporter, feColorMatrixResult);
+
+            if (!knockout && !compositeSource) {
+                return blurResult;
+            }
+
+            Element feComposite = document.createElement("feComposite");
+            if (knockout) {
+                feComposite.setAttribute("in", blurResult);
+                feComposite.setAttribute("in2", in);
+                feComposite.setAttribute("operator", "out");
+            } else {
+                feComposite.setAttribute("in", in);
+                feComposite.setAttribute("in2", blurResult);
+                feComposite.setAttribute("operator", "over");
+            }
+
+            String feCompositeResult = exporter.getUniqueId("filterResult");
+            feComposite.setAttribute("result", feCompositeResult);
+            filtersElement.appendChild(feComposite);
+            return feCompositeResult;
+        }
     }
 }
