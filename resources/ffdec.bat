9170fef3cd2dcc1fc6e1b28790cc82b76cc36ea6
Jindra Petřík <jindra.petrik@gmail.com>
2023-11-20 18:47:11 +0100

    fix ffdec.bat

--- resources/ffdec.bat
+++ resources/ffdec.bat
@@ -12,9 +12,11 @@ rem set FFDEC_STACK_SIZE=32m
 rem Hide VLC error output
 set VLC_VERBOSE=-1
 
+if "%FFDEC_MEMORY%"=="" set FFDEC_MEMORY=1024m
+
 set STACK_SIZE_PARAM=
 set MEMORY_PARAM=
 if not "%FFDEC_STACK_SiZE%"=="" set STACK_SIZE_PARAM= -Xss%FFDEC_STACK_SiZE% 
 if not "%FFDEC_MEMORY%"=="" set MEMORY_PARAM=-Xmx%FFDEC_MEMORY% 
 
-echo %MEMORY_PARAM%%STACK_SIZE_PARAM%-Djna.nosys=true -jar "%~dp0\ffdec.jar" %*
+java %MEMORY_PARAM%%STACK_SIZE_PARAM%-Djna.nosys=true -jar "%~dp0\ffdec.jar" %*
