4290b8c9430c6f0e2588bb481523cc5f1e315b5b
Jindra Petřík <jindra.petrik@gmail.com>
2023-12-31 00:51:47 +0100

    harman encryption test data [ci skip]

---
+++ libsrc/ffdec_lib/testdata/as3_harman/build_harman_debug.bat
@@ -0,0 +1,6 @@
+@echo off
+set MY_PATH=%~dp0
+call c:\air_harman\bin\mxmlc.bat -debug=true -output bin/harman.swf src/Main.as 1> buildlog.harman.txt 2>&1
+cd c:\air_harman\bin\
+call swfencrypt.bat -in %MY_PATH%\bin\harman.swf -out %MY_PATH%\bin\harman_encrypted.swf
+
