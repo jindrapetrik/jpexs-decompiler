ce648aeb458b195a34c9c8508f30b6c3e35c4394
Jindra Petřík <jindra.petrik@gmail.com>
2024-01-21 13:30:41 +0100

    remove command stub

---
+++ libsrc/ffdec_cli/src/com/jpexs/decompiler/flash/cli/commands/Remove.java
@@ -0,0 +1,61 @@
+package com.jpexs.decompiler.flash.cli.commands;
+
+import com.jpexs.decompiler.flash.cli.VersionProvider;
+import com.jpexs.decompiler.flash.cli.commands.types.CompressionKind;
+import com.jpexs.decompiler.flash.cli.commands.types.ReplaceFormat;
+import java.util.List;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
+import picocli.CommandLine.ArgGroup;
+
+/**
+ *
+ * @author JPEXS
+ */
+@Command(
+        name = "remove",
+        mixinStandardHelpOptions = true,
+        versionProvider = VersionProvider.class,
+        header = "Removes a tag from the SWF",
+        optionListHeading = "%n@|bold,underline Options|@:%n",           
+        parameterListHeading = "%n@|bold,underline Parameters|@:%n",
+        synopsisHeading = "@|bold,underline Usage|@:",
+        footerHeading = "%n@|bold,underline Examples|@:%n",
+        footer = {
+            "ffdec-cli remove --tag-index=12 --tag-index=13 input.swf output.swf",
+            "ffdec-cli remove --tag-index=5 input.swf output.swf"
+        },
+        sortSynopsis = false
+)
+public class Remove implements Runnable {      
+    @Parameters(
+            index = "0",
+            paramLabel = "IN_FILE",
+            description = "Input file"
+    )
+    String inFile;
+    
+    @Parameters(
+            index = "1",
+            paramLabel = "OUT_FILE",
+            description = "Output file"
+    )
+    String outFile;
+    
+    @Option(
+            names = "--tag-index",
+            description = "Index of tag on SWF timeline",
+            paramLabel = "<tagIndex>",
+            required = true,
+            arity = "1..*"
+    )
+    List<Integer> tagIndices;
+    
+    @Override
+    public void run() {
+        
+    }
+}
