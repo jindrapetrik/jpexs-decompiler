a9f339e5e6868f1842dda3344b2bc1f04ab78a00
Jindra Petřík <jindra.petrik@gmail.com>
2024-01-21 16:26:04 +0100

    abcmerge command stub

---
+++ libsrc/ffdec_cli/src/com/jpexs/decompiler/flash/cli/commands/AbcMerge.java
@@ -0,0 +1,46 @@
+package com.jpexs.decompiler.flash.cli.commands;
+
+import com.jpexs.decompiler.flash.cli.VersionProvider;
+import com.jpexs.decompiler.flash.cli.commands.types.CompressionKind;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
+
+/**
+ *
+ * @author JPEXS
+ */
+@Command(
+        name = "abcmerge",
+        mixinStandardHelpOptions = true,
+        versionProvider = VersionProvider.class,
+        header = "Merge all ABC tags in SWF file to one",
+        parameterListHeading = "%n@|bold,underline Parameters|@:%n",
+        synopsisHeading = "@|bold,underline Usage|@:",
+        footerHeading = "%n@|bold,underline Example|@:%n",
+        footer = {
+            "ffdec-cli abcmerge input.swf output.swf",
+        }
+)
+public class AbcMerge implements Runnable {
+    @Parameters(
+            index = "0",
+            paramLabel = "IN_FILE",
+            description = "Input file"
+    )
+    String inFile;
+    
+    @Parameters(
+            index = "1",
+            paramLabel = "OUT_FILE",
+            description = "Output file"
+    )
+    String outFile;
+
+    @Override
+    public void run() {
+     
+    }
+}
