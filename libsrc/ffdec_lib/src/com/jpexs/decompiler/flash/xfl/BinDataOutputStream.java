a4e08e50e9c0242792ebd627e6bd0216f6d92976
Jindra Petřík <jindra.petrik@gmail.com>
2024-09-07 19:51:41 +0200

    Added: FLA export - generating bin/*.dat files for movies and images

---
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/xfl/BinDataOutputStream.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (C) 2010-2024 JPEXS, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.jpexs.decompiler.flash.xfl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author JPEXS
+ */
+public class BinDataOutputStream extends OutputStream {
+
+    private final OutputStream os;
+
+    public BinDataOutputStream(OutputStream os) {
+        this.os = os;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        os.write(b);
+    }
+
+    public void write(int... values) throws IOException {
+        for (int i : values) {
+            os.write(i);
+        }
+    }
+
+    public void writeUI16(int value) throws IOException {
+        write(value & 0xFF);
+        write((value >> 8) & 0xFF);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        os.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        os.write(b, off, len);
+    }
+
+    public void writeUI32(long value) throws IOException {
+        write((int) (value & 0xFF));
+        write((int) ((value >> 8) & 0xFF));
+        write((int) ((value >> 16) & 0xFF));
+        write((int) ((value >> 24) & 0xFF));
+    }
+
+    public void writeUI64(long value) throws IOException {
+        write((int) (value & 0xFF));
+        write((int) ((value >> 8) & 0xFF));
+        write((int) ((value >> 16) & 0xFF));
+        write((int) ((value >> 24) & 0xFF));
+        write((int) ((value >> 32) & 0xFF));
+        write((int) ((value >> 40) & 0xFF));
+        write((int) ((value >> 48) & 0xFF));
+        write((int) ((value >> 56) & 0xFF));
+    }
+
+    public void writeFloat(float val) throws IOException {
+        writeUI32(Float.floatToIntBits(val));
+    }
+
+    public void writeDouble(double val) throws IOException {
+        writeUI64(Double.doubleToLongBits(val));
+    }
+}
---
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/xfl/ImageBinDataGenerator.java
@@ -0,0 +1,98 @@
+/*
+ *  Copyright (C) 2010-2024 JPEXS, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.jpexs.decompiler.flash.xfl;
+
+import com.jpexs.decompiler.flash.tags.enums.ImageFormat;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author JPEXS
+ */
+public class ImageBinDataGenerator {
+
+    public void generateBinData(InputStream is, OutputStream os, ImageFormat format) throws IOException {
+        BufferedImage bimg = ImageIO.read(is);
+        if (format == ImageFormat.JPEG) {
+            byte[] buf = new byte[4096];
+            int cnt;
+            while ((cnt = is.read(buf)) > 0) {
+                os.write(buf, 0, cnt);
+            }
+            BinDataOutputStream dw2 = new BinDataOutputStream(os);
+            dw2.writeUI32(0);
+            dw2.writeUI32(0);
+            dw2.writeUI32(20 * bimg.getWidth());
+            dw2.writeUI32(20 * bimg.getHeight());
+        } else {
+            //https://stackoverflow.com/questions/4082812/xfl-what-are-the-bin-dat-files/4082907#4082907
+            os.write(0x03);
+            os.write(0x05);
+            BinDataOutputStream w = new BinDataOutputStream(os);
+
+            int decRowLen = 4 * bimg.getWidth();
+            w.writeUI16(decRowLen);
+
+            w.writeUI16(bimg.getWidth());
+            w.writeUI16(bimg.getHeight());
+
+            w.writeUI32(0);
+            w.writeUI32(0);
+            w.writeUI32(20 * bimg.getWidth());
+            w.writeUI32(20 * bimg.getHeight());
+
+            w.write(0x01); //has transparency
+            w.write(0x01); //compressed variant
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DeflaterOutputStream def = new DeflaterOutputStream(baos, new Deflater(1));
+
+            for (int y = 0; y < bimg.getHeight(); y++) {
+                for (int x = 0; x < bimg.getWidth(); x++) {
+                    int rgba = bimg.getRGB(x, y);
+                    def.write((rgba >> 24) & 0xFF); //a 
+                    def.write((rgba >> 16) & 0xFF); //b
+                    def.write((rgba >> 8) & 0xFF); //g
+                    def.write(rgba & 0xFF); //r                                                       
+                }
+            }
+            def.flush();
+            def.finish();
+            byte[] data = baos.toByteArray();
+            int pos = 0;
+            while (pos < data.length) {
+                int cnt = 2048; //it seems that using large chunk sizes like 0xFFFF crashes flash. 2024 is used in CS5.
+                if (pos + cnt > data.length) {
+                    cnt = data.length - pos;
+                }
+                w.writeUI16(cnt);
+                os.write(data, pos, cnt);
+                pos += cnt;
+            }
+            w.writeUI16(0);
+        }
+    }
+}
