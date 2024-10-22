65b979535f86fd74f31ba8b65ee386bc1b395112
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-10-22 13:10:30 -0400

    spelling: sound
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/tags/SoundStreamHead2Tag.java
+++ libsrc/ffdec_lib/src/com/jpexs/decompiler/flash/tags/SoundStreamHead2Tag.java
@@ -37,7 +37,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * SooundStreamHead2 tag - contains sound stream information for streaming
+ * SoundStreamHead2 tag - contains sound stream information for streaming
  * sound. Extends functionality of SoundStreamHead tag.
  *
  * @author JPEXS
