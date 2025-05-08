86d21a130864b2f624978d15843fc8a5f740185b
Jindra Petřík <jindra.petrik@gmail.com>
2025-05-08 19:47:46 +0200

    CheckboxMenuItems instead Checkboxes in Snap options button. Moved Snap options button.

--- src/com/jpexs/decompiler/flash/gui/player/SnapOptionsButton.java
+++ src/com/jpexs/decompiler/flash/gui/player/SnapOptionsButton.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.jpexs.decompiler.flash.gui.abc;
+package com.jpexs.decompiler.flash.gui.player;
 
 import com.jpexs.decompiler.flash.configuration.Configuration;
 import com.jpexs.decompiler.flash.gui.AppStrings;
@@ -24,7 +24,7 @@ import com.jpexs.decompiler.flash.gui.SnappingDialog;
 import com.jpexs.decompiler.flash.gui.View;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
@@ -37,35 +37,34 @@ public class SnapOptionsButton extends PopupButton {
     
     public SnapOptionsButton() {                
         super(View.getIcon("snap16"));        
-        setToolTipText(AppStrings.translate("button.snap_options"));
-        setMargin(new Insets(0, 0, 0, 0));        
+        setToolTipText(AppStrings.translate("button.snap_options"));        
     }
 
     @Override
     protected JPopupMenu getPopupMenu() {
         JPopupMenu popupMenu = new JPopupMenu();
         
-        JCheckBox snapAlignMenuItem = new JCheckBox(AppStrings.translate("snap_options.snap_align"));
+        JCheckBoxMenuItem snapAlignMenuItem = new JCheckBoxMenuItem(AppStrings.translate("snap_options.snap_align"));
         snapAlignMenuItem.setSelected(Configuration.snapAlign.get());
         snapAlignMenuItem.addActionListener(this::snapAlignMenuItemActionPerformed);
         popupMenu.add(snapAlignMenuItem);
         
-        JCheckBox snapToGridMenuItem = new JCheckBox(AppStrings.translate("snap_options.snap_to_grid"));
+        JCheckBoxMenuItem snapToGridMenuItem = new JCheckBoxMenuItem(AppStrings.translate("snap_options.snap_to_grid"));
         snapToGridMenuItem.setSelected(Configuration.snapToGrid.get());
         snapToGridMenuItem.addActionListener(this::snapToGridMenuItemActionPerformed);
         popupMenu.add(snapToGridMenuItem);
         
-        JCheckBox snapToGuidesMenuItem = new JCheckBox(AppStrings.translate("snap_options.snap_to_guides"));
+        JCheckBoxMenuItem snapToGuidesMenuItem = new JCheckBoxMenuItem(AppStrings.translate("snap_options.snap_to_guides"));
         snapToGuidesMenuItem.setSelected(Configuration.snapToGuides.get());
         snapToGuidesMenuItem.addActionListener(this::snapToGuidesMenuItemActionPerformed);
         popupMenu.add(snapToGuidesMenuItem);
         
-        JCheckBox snapToPixelsMenuItem = new JCheckBox(AppStrings.translate("snap_options.snap_to_pixels"));
+        JCheckBoxMenuItem snapToPixelsMenuItem = new JCheckBoxMenuItem(AppStrings.translate("snap_options.snap_to_pixels"));
         snapToPixelsMenuItem.setSelected(Configuration.snapToPixels.get());
         snapToPixelsMenuItem.addActionListener(this::snapToPixelsMenuItemActionPerformed);
         popupMenu.add(snapToPixelsMenuItem);
         
-        JCheckBox snapToObjectsMenuItem = new JCheckBox(AppStrings.translate("snap_options.snap_to_objects"));
+        JCheckBoxMenuItem snapToObjectsMenuItem = new JCheckBoxMenuItem(AppStrings.translate("snap_options.snap_to_objects"));
         snapToObjectsMenuItem.setSelected(Configuration.snapToObjects.get());
         snapToObjectsMenuItem.addActionListener(this::snapToObjectsMenuItemActionPerformed);
         popupMenu.add(snapToObjectsMenuItem);
@@ -82,27 +81,27 @@ public class SnapOptionsButton extends PopupButton {
     }
     
     private void snapAlignMenuItemActionPerformed(ActionEvent evt) {
-        JCheckBox menuItem = (JCheckBox) evt.getSource();
+        JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) evt.getSource();
         Configuration.snapAlign.set(menuItem.isSelected());        
     }
     
     private void snapToGridMenuItemActionPerformed(ActionEvent evt) {
-        JCheckBox menuItem = (JCheckBox) evt.getSource();
+        JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) evt.getSource();
         Configuration.snapToGrid.set(menuItem.isSelected());        
     }
     
     private void snapToGuidesMenuItemActionPerformed(ActionEvent evt) {
-        JCheckBox menuItem = (JCheckBox) evt.getSource();
+        JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) evt.getSource();
         Configuration.snapToGuides.set(menuItem.isSelected());        
     }
     
     private void snapToPixelsMenuItemActionPerformed(ActionEvent evt) {
-        JCheckBox menuItem = (JCheckBox) evt.getSource();
+        JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) evt.getSource();
         Configuration.snapToPixels.set(menuItem.isSelected());        
     }
     
     private void snapToObjectsMenuItemActionPerformed(ActionEvent evt) {
-        JCheckBox menuItem = (JCheckBox) evt.getSource();
+        JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) evt.getSource();
         Configuration.snapToObjects.set(menuItem.isSelected());        
     }
     
