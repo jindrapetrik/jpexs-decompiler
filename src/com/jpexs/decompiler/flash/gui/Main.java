aa923dd201f1cd6cd0534732b8f521b2c098be68
Jindra Petřík <jindra.petrik@gmail.com>
2026-04-12 18:00:25 +0200

    fix: fix nightly update checking showing current version

--- src/com/jpexs/decompiler/flash/gui/Main.java
+++ src/com/jpexs/decompiler/flash/gui/Main.java
@@ -3892,7 +3892,7 @@ public class Main {
         for (int i = 0; i < arr.size(); i++) {
             JsonObject versionObj = arr.get(i).asObject();
             String tagName = versionObj.get("tag_name").asString();
-            if (currentVersion.equals(tagName) || stableTagName.equals(tagName)) {
+            if (currentTagName.equals(tagName) || stableTagName.equals(tagName)) {
                 //Stop at current version, do not display more
                 break;
             }
